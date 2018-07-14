5f6ba9dc6f10afced292dd49e9b424744bc728b8
LifeGoal <35453587+LifeGoal@users.noreply.github.com>
2018-07-14 21:35:59 +0200

    Update eden_weapon_accesories.sql to new version
    Now in English
    
    Now working with the latest items table

--- eden_weapon_accesories.sql
+++ eden_weapon_accesories.sql
@@ -1,4 +1,4 @@
-INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES (2010, 'yusuf', 'Skin de luxe', -1, 0, 1);
-INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES (209, 'grip', 'Poignée', -1, 0, 1);
-INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES (208, 'flashlight', 'Lampe', -1, 0, 1);
-INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES (207, 'silencieux', 'Silencieux', -1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('yusuf', 'Deluxe Skin', -1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('grip', 'Grip', -1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('flashlight', 'Lamp', -1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('silencieux', 'Silencer', -1, 0, 1);
